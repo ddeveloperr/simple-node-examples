568afd57ad57b29ac70db312bcc4f0eb25926af4
KemalC <ddeveloperr@gmail.com>
2017-07-04 18:44:57 +0000

    #1 Simple echo project

---
+++ echo/echo.js
@@ -0,0 +1,9 @@
+function echo(str, num){
+    for(var i = 0; i <= num; i++)
+    console.log(str);
+}
+
+echo("This is my Echo called from echo function", 10);
+echo("Echo: Node.js is so fun!", 7);
+
+
