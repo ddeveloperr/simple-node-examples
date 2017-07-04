5865cd298d1f3c7b0a053797da01ef1454b4ddac
KemalC <ddeveloperr@gmail.com>
2017-07-04 22:52:09 +0000

    #3 Demo and Fake data Project

---
+++ demo-node-app/app.js
@@ -0,0 +1,12 @@
+var catMe = require('cat-me');
+var joke = require('knock-knock-jokes');
+ 
+// Give me awesome random cat :)
+console.log(catMe());
+
+console.log("From new app.js voice!");
+
+
+var knockknock = require('knock-knock-jokes');
+ 
+console.log(knockknock()); // returns a knock knock joke 
